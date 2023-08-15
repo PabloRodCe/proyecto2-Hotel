c370eb7941bec02dfaf4917806a20f14d0d0b424
pablorod <jorodriguezce@est.utn.ac.cr>
2023-08-14 21:11:17 -0600

    se crea el proyecto
      y las primeras clases

---
+++ src/AdmiEmpleados/Empleados.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package AdmiEmpleados;
+
+/**
+ *
+ * @author Pablo Rod
+ */
+public class Empleados {
+    
+}
---
+++ src/AdmiServicios/Servicios.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package AdmiServicios;
+
+/**
+ *
+ * @author Pablo Rod
+ */
+public class Servicios {
+    
+}
