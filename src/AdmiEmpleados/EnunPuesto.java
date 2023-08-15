f929bc79b39967789dcbdfdf2e29cd4bc6738679
pablorod <jorodriguezce@est.utn.ac.cr>
2023-08-15 00:07:37 -0600

    se crea el enun
     se crea el enum de los puestos

---
+++ src/AdmiEmpleados/EnunPuesto.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package AdmiEmpleados;
+
+/**
+ *
+ * @author Pablo Rod
+ */
+public enum EnunPuesto {
+    Recepcionista,
+    Gerente,
+    Conserge,
+    Supervisor,
+    Mantenimiento;
+    
+}
