1d368f7ba3d26efd61dca172d70290fc69ad448f
pablorod <jorodriguezce@est.utn.ac.cr>
2023-08-19 12:21:08 -0600

    se añade empleados controller
    se añade la clase empleadosController

---
+++ src/AdmiEmpleados/EmpleadosController.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package AdmiEmpleados;
+
+import Inter_Faces.Controller;
+
+/**
+ *
+ * @author Pablo Rod
+ */
+public class EmpleadosController implements Controller<Empleados> {
+
+    @Override
+    public void agregar(Empleados obj) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void eliminar(Empleados obj) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void buscar(Empleados obj) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void actualizar(Empleados obj) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
