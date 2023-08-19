8775f104a3762f74c67f874033ec5d037ab8811d
isaacsibajamora <isaacsibaja901@gmail.com>
2023-08-19 12:31:21 -0600

    paquete excepciones
     se agrega paquete exepciones

---
+++ src/Excepciones/ReservasPendientesException.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Excepciones;
+
+/**
+ *
+ * @author Isaac Sibaja
+ */
+public class ReservasPendientesException extends Exception {
+    public ReservasPendientesException(String message){
+        super("for negative value");
+        
+}
+    }
+
