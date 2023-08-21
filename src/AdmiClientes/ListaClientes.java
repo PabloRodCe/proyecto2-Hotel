071a6d9b28ae2286fdb292c3f3d51b46358a5ffd
isaacsibajamora <isaacsibaja901@gmail.com>
2023-08-20 18:39:34 -0600

    se modico un metodo
     hubo mdificacion

--- src/AdmiClientes/ListaClientes.java
+++ src/AdmiClientes/ListaClientes.java
@@ -37,8 +37,8 @@ public class ListaClientes implements List<Cliente>{
     }
 
     @Override
-    public boolean eliminar(Cliente obj) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public boolean eliminar(Cliente obj) {     
+       throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
     @Override
